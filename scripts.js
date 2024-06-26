99c1f3c3b3c247931c1182a044eee12d857be50d
Tosirama <55952723+Tosirama@users.noreply.github.com>
2024-06-26 09:38:46 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,14 @@
+const fb2 = document.getElementById('fb2');
+const im2 = document.getElementById('im2');
+
+fb2.addEventListener('click',function(){
+    im2.style.height='300px';
+    im2.style.width= '300px';
+
+
+})
+
+fb2.addEventListener('dblclick',function(){
+    im2.style.height='0';
+    im2.style.width= '0';
+})
